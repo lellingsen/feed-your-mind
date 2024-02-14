466b9909208cd63637b2e355b22fff38e3149778
Lars Ellingsen <lellingsen17@gmail.com>
2024-02-13 21:03:04 -0800

    Starting to fetch RSS feeds, realizing that CORS is going to be a problem

--- src/domains-signal.ts
+++ src/domains-signal.ts
@@ -1,3 +1,20 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 
 export const [domains, setDomains] = createSignal<string[]>([]);
+export const [feeds, setFeeds] = createSignal<any[]>([]);
+
+createEffect(async () => {
+  const domainList = domains();
+  const promises = domainList.map(async (domain) => {
+    const response = await fetch(`${domain}/rss.xml`);
+    if (response.ok) {
+      const responseText = await response.text();
+      const parser = new window.DOMParser();
+      const rssXml = parser.parseFromString(responseText, "text/xml");
+      console.log(rssXml);
+    } else {
+      console.log(`Failed to fetch feed for ${domain}`);
+    }
+  });
+  await Promise.all(promises);
+});
